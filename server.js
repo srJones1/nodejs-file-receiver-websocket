d5d55e4787a233ae5cb81576677d88f02dc75aff
srJones1 <67037530+srJones1@users.noreply.github.com>
2023-04-01 08:30:45 -0500

    Update server.js

--- server.js
+++ server.js
@@ -6,10 +6,15 @@ const server = require('http').Server(app)
 const io = require('socket.io')(server,{maxHttpBufferSize: 1e7})
 
 io.on('connection', function (socket) {
+  console.log("headers at connection:")
+  console.log(socket.handshake.headers)
   socket.on('audio', async function (msg) {
+      console.log("headers on evnt:")
+      console.log(socket.handshake.headers)
       var date = new Date(msg.timeMillis);
       await fs.promises.writeFile(path.join(__dirname, "videos", `${date.toString()}.wav`), msg.data, "binary");         
   });
 });
 
 server.listen(3000)
+console.log("Running on port: "+3000)
